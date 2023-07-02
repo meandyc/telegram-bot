b3c28135a31263497162f40b5d2327baba0e8e85
Andy <tsudon@duck.com>
2023-07-02 18:20:55 -0400

    Second commit

--- telegram-bot.py
+++ telegram-bot.py
@@ -9,11 +9,11 @@ from chat import Chat
 from user import User
 
 class TelegramBot:
-        
     def __init__(self):
         self.API_URL = 'https://api.telegram.org/bot'
         with open('/home/andy/Documents/Python/telegram-bot/.telegram-api-token', 'r') as f:
             self.API_ID = f.read()
+        self.updates_offset = None
     
     def get_updates(self):
         """
@@ -21,11 +21,14 @@ class TelegramBot:
         Returns an Array of Update objects.
         """
         url = self.API_URL + self.API_ID + '/getUpdates'
-        response = requests.get(url)
+        params = {'offset': self.updates_offset, 'timeout': 2}
+        response = requests.get(url, offset=self.updates_offset)
         if response.status_code == 200:
             updates = json.loads(response.text)
             if updates['ok']:
-                return updates['result']
+                result = updates['result']
+                self.updates_offset = result[-1]['update_id'] + 1
+                return result
         else:
             print('Failed getting updates')
 
@@ -34,21 +37,24 @@ class TelegramBot:
         Remove messages with banned words.
         """
         for update in updates:
-            # pprint(update)
-            print(f'message_id: {update["message"]["message_id"]}')
-            print(f'chat_id: {update["message"]["chat"]["id"]}')
-            print(f'text: {update["message"]["text"]}')
-            print('---')
+            # print(f'offset in remove_banned_words: {self.updates_offset}')
+            
+            message_id = update["message"]["message_id"]
+            chat_id = update["message"]["chat"]["id"]
+            text = update["message"]["text"]
+            if 'test' in text.lower():
+                url = self.API_URL + self.API_ID + '/deleteMessage'
+                params = {'chat_id': chat_id, 'message_id': message_id}
+                # print(params)
+                response = requests.post(url, data=params)
+                # response.raise_for_status()
+                if response.status_code == 200:
+                    result = json.loads(response.text)
+                    
+                else:
+                    print(f'Couldn\'t delete the message "{text} {chat_id} {message_id}".')
 
-            # url = self.API_URL + self.API_ID + '/deleteMessage'
-            # params = {'chat_id': '-991250963', 'message_id': '81'}
-            # response = requests.post(url, data=params)
-            # if response.status_code == 200:
-            #     result = json.loads(response.text)
-            # else:
-            #     print('Couldn\'t delete the message.')
-
-    def send_message(self, chat_id='969768646', msg='Test message Telegram API'):
+    def send_message(self, chat_id='969768646', msg='Test message. Telegram API'):
         """
         Use this method to send text messages. On success, the sent Message is returned.
         """
@@ -68,5 +74,5 @@ if __name__ == '__main__':
     # Pablo 627140333
     
     bot = TelegramBot()
-    # bot.send_message()
+    # bot.send_message(-991250963)
     bot.remove_banned_words(bot.get_updates())
