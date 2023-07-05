160c9cd9f6153594650f465e6238ebafcff5a80a
Andy <tsudon@duck.com>
2023-07-05 11:58:11 -0400

    Added bold formatting to the name of the downloaded Torrent file.

--- telegram-bot.py
+++ telegram-bot.py
@@ -18,8 +18,8 @@ class TelegramBot:
         """
         url = self.API_URL + self.API_ID + '/sendMessage'
         current_time = datetime.datetime.fromtimestamp(time.time())
-        msg = '"' + torrent_name + '" downloaded at ' +  current_time.strftime("%H:%M.")
-        params = {'chat_id': chat_id, 'text': msg}
+        msg = '"<b>' + torrent_name + '</b>" downloaded at ' +  current_time.strftime("%H:%M.")
+        params = {'chat_id': chat_id, 'text': msg, 'parse_mode': 'html'}
         response = requests.post(url, data=params)
         if response.status_code == 200:
             json_response = json.loads(response.text)
